692ee1fc805a95895b0c1442afef4f296bba08f0
Guilherme Gouveia <guilherme.rgouveia@gmail.com>
2023-02-12 15:29:53 -0400

    Add script to create bootstrap cluster

---
+++ create-kind-cluster.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+cat <<EOF > kind-cluster-config.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+
+kind create cluster --name capz --config ./kind-cluster-config.yaml
+
